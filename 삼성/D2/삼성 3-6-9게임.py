567687e8ef847d83ae4d2de88078ee02a53c24ca
JI HYUN SIK <59475851+HYUNSIK-JI@users.noreply.github.com>
2022-07-21 14:13:15 +0900

    Delete 삼성 3-6-9게임.py

--- 삼성/D2/삼성 3-6-9게임.py
+++
@@ -1,8 +0,0 @@
-for i in range(1,int(input())+1):
-    i=str(i)
-    cnt=i.count("3")+i.count("6")+i.count("9")
-
-    if cnt>0:
-        print("-"*cnt ,end=" ")
-    else:
-        print(i,end=" ")
