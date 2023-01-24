2db5ca60db8f8d1b7de42cdc1c7103a74b7856ea
JI HYUN SIK <wlgustlraaa@naver.com>
2023-01-24 16:40:17 +0900

    Update 호석사우루스.py

--- 백준/Gold/22255. 호석사우루스/호석사우루스.py
+++ 백준/Gold/22255. 호석사우루스/호석사우루스.py
@@ -4,60 +4,54 @@ from heapq import heappush, heappop
 INF = sys.maxsize
 input = sys.stdin.readline
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
+class Solution():
+    def __init__(self):
+        self.N = list(map(int, input().split()))
+        self.position = list(map(int, input().split()))
+        self.queue = []
+        self.maps = [list(map(int, input().split())) for _ in range(self.N[0])]
+        self.dis = [[[INF] * 3 for _ in range(self.N[1])] for _ in range(self.N[0])]
+        self.dx = [1, -1, 0, 0]
+        self.dy = [0, 0, -1, 1]
 
-def dijstra(a, b, start):
-    queue = []
-    heappush(queue, (0, start, a, b))
-    dis[a][b][1] = 0
+    def cal(self, nx, ny, dist, c):
+        if 0 <= nx < self.N[0] and 0 <= ny < self.N[1] and not self.maps[nx][ny] == -1:
+            cost = dist + self.maps[nx][ny]
 
-    while queue:
-        dist, c, x, y = heappop(queue)
+            if cost < self.dis[nx][ny][(c + 1) % 3]:
+                self.dis[nx][ny][(c + 1) % 3] = cost
+                heappush(self.queue, (cost, c + 1, nx, ny))
 
-        if x == (e_x - 1) and y == (e_y - 1):
-            return dist
+    def dijstra(self):
+        heappush(self.queue, (0, 1, self.position[0] - 1, self.position[1] - 1))
+        self.dis[self.position[0] - 1][self.position[1] - 1][1] = 0
 
-        if not c % 3:
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
+        while self.queue:
+            dist, c, x, y = heappop(self.queue)
 
-                if 0 <= nx < n and 0 <= ny < m and not maps[nx][ny] == -1:
-                    cost = dist + maps[nx][ny]
+            if x == (self.position[2] - 1) and y == (self.position[3] - 1):
+                return dist
 
-                    if cost < dis[nx][ny][(c + 1) % 3]:
-                        dis[nx][ny][(c + 1) % 3] = cost
-                        heappush(queue, (cost, c + 1, nx, ny))
+            if not c % 3:
+                for i in range(4):
+                    nx = x + self.dx[i]
+                    ny = y + self.dy[i]
 
-        if c % 3 == 1:
-            for i in range(2):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < n and 0 <= ny < m and not maps[nx][ny] == -1:
-                    cost = dist + maps[nx][ny]
+                    self.cal(nx, ny, dist, c)
 
-                    if cost < dis[nx][ny][(c + 1) % 3]:
-                        dis[nx][ny][(c + 1) % 3] = cost
-                        heappush(queue, (cost, c + 1, nx, ny))
-        else:
-            for i in range(2, 4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < n and 0 <= ny < m and not maps[nx][ny] == -1:
-                    cost = dist + maps[nx][ny]
+            elif c % 3 == 1:
+                for i in range(2):
+                    nx = x + self.dx[i]
+                    ny = y + self.dy[i]
 
-                    if cost < dis[nx][ny][(c + 1) % 3]:
-                        dis[nx][ny][(c + 1) % 3] = cost
-                        heappush(queue, (cost, c + 1, nx, ny))
-    return -1
+                    self.cal(nx, ny, dist, c)
+            else:
+                for i in range(2, 4):
+                    nx = x + self.dx[i]
+                    ny = y + self.dy[i]
 
-n, m = map(int, input().split())
+                    self.cal(nx, ny, dist, c)
+        return -1
 
-s_x, s_y, e_x, e_y = map(int, input().split())
-
-maps = [list(map(int, input().split())) for _ in range(n)]
-
-dis = [[[INF] * 3 for _ in range(m)] for _ in range(n)]
-
-print(dijstra(s_x - 1, s_y - 1, 1))
+answer = Solution()
+print(answer.dijstra())
