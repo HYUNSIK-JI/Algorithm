16fa58378a20fbd855ecc6afeb38fd2edffa7a2d
JI HYUN SIK <wlgustlraaa@naver.com>
2023-01-29 18:53:46 +0900

    [Silver I] Title: RGB거리, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,12 +1,14 @@
 import sys
-n=int(sys.stdin.readline())
-dp=[]
 
-for i in range(n):
-	dp.append(list(map(int,sys.stdin.readline().split())))
+input = sys.stdin.readline
 
-for i in range(1,len(dp)):
-	dp[i][0]=dp[i][0]+min(dp[i-1][1],dp[i-1][2])
-	dp[i][1]=dp[i][1]+min(dp[i-1][0],dp[i-1][2])
-	dp[i][2]=dp[i][2]+min(dp[i-1][0],dp[i-1][1])
-print(min(dp[n-1][0],dp[n-1][1],dp[n-1][2]))
+n = int(input())
+
+dp = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(1, n):
+    dp[i][0] = dp[i][0] + min(dp[i - 1][1], dp[i - 1][2])
+    dp[i][1] = dp[i][1] + min(dp[i - 1][0], dp[i - 1][2])
+    dp[i][2] = dp[i][2] + min(dp[i - 1][0], dp[i - 1][1])
+
+print(min(dp[n - 1]))
