dbbe4d25b27c6735f426b5a0e018b10bf30d374d
JI HYUN SIK <wlgustlraaa@naver.com>
2023-02-05 01:15:01 +0900

    Update 등굣길.py

--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -1,7 +1,5 @@
 import sys
 
-sys.setrecursionlimit(10 ** 8)
-
 dx = [0, 1]
 dy = [1, 0]
 
@@ -22,11 +20,12 @@ def dfs(x, y, dp, m, n, maps):
                 ways += dfs(nx, ny, dp, m, n, maps)
     dp[x][y] = ways
     return dp[x][y] % 1000000007
+
 def solution(m, n, puddles):
     maps = [[1] * m for _ in range(n)]
     
     for x, y in puddles:
         maps[y - 1][x - 1] = 0
+    
     dp = [[-1] * m for _ in range(n)]
     return(dfs(0, 0, dp, m, n, maps))
-    
