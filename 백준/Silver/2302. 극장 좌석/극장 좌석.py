2a06ccf89399d0f307b71a11b8c8c4ed69ddf204
JI HYUN SIK <wlgustlraaa@naver.com>
2023-02-04 21:50:46 +0900

    Update 극장 좌석.py

--- 백준/Silver/2302. 극장 좌석/극장 좌석.py
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.py
@@ -4,23 +4,28 @@ input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-vip = [int(input()) for _ in range(m)]
 
-dp = [0] * (n + 1)
+check = [False] * (n + 1)
+
+dp = [0] * 41
+for i in range(m):
+    check[int(input())] = True
+
 dp[0] = 1
 dp[1] = 1
 
 for i in range(2, n + 1):
     dp[i] = dp[i - 1] + dp[i - 2]
 
+cnt = 0
 ans = 1
-
-if m:
-    previous = 0
-    for i in range(m):
-        ans *= dp[vip[i] - 1 - previous]
-        previous = vip[i]
-    ans *= dp[n - previous]
-    print(ans)
-else:
-    print(dp[n])
+for i in range(1, n + 1):
+    if not check[i]:
+        cnt += 1
+    else:
+        if cnt:
+            ans *= dp[cnt]
+        cnt = 0
+if cnt:
+    ans *= dp[cnt]
+print(ans)
