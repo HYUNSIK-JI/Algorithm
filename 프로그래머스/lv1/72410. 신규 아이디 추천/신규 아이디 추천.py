e7092b1823aa27ba4d42e6258135acfcb32e0ceb
JI HYUN SIK <wlgustlraaa@naver.com>
2023-06-17 17:28:20 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,50 +1,14 @@
 import re
 
-def step_one(data):
-    return data.lower()
-def step_two(data):
-    accecpt_str = ['.', '-', '_']
-    ans = []
-    for i in data:
-        if i.isdigit() or i.isalpha() or i in accecpt_str:
-            ans.append(i)
-        elif not i in accecpt_str:
-            continue
-    return "".join(ans)
-def step_three(data):
-    data = re.sub(r'\.+', '.', data)
-    return data
-
-def step_four(data):
-    if data[0] == "." and data[-1] != ".":
-        data = data[1:]
-    elif data[0] != "." and data[-1] == ".":
-        data = data[:-1]
-    elif data[0] == "." and data[-1] == ".":
-        data = data[1:-1]
-    return data
-
-def step_five(data):
-    if not data:
-        data += "a"
-    return data
-def step_six(data):
-    ans = []
-    if len(data) >= 16:
-        data = data[:15]
-    if data[-1] == ".":
-        data = data[:-1]
-    return data
-def step_seven(data):
-    while len(data) < 3:
-        data += data[-1]
-    return data
 def solution(new_id):
-    answer = step_one(new_id)
-    answer = step_two(answer)
-    answer = step_three(answer)
-    answer = step_four(answer)
-    answer = step_five(answer)
-    answer = step_six(answer)
-    answer = step_seven(answer)
+    st = new_id
+    st = st.lower()
+    st = re.sub('[^a-z0-9\-_.]', '', st)
+    st = re.sub('\.+', '.', st)
+    st = re.sub('^[.]|[.]$', '', st)
+    st = 'a' if len(st) == 0 else st[:15]
+    st = re.sub('^[.]|[.]$', '', st)
+    st = st if len(st) > 2 else st + "".join([st[-1] for i in range(3-len(st))])
+    return st
+
     return answer
