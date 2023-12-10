44a40c1a5fadf9867ec4963cd55335434e09858a
JI HYUN SIK <wlgustlraaa@naver.com>
2023-12-10 14:24:02 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -0,0 +1,12 @@
+def solution(dot):
+    x = dot[0]
+    y = dot[1]
+    
+    if x > 0 and y > 0:
+        return 1
+    elif x > 0 and y < 0:
+        return 4
+    elif x < 0 and y > 0:
+        return 2
+    else:
+        return 3
