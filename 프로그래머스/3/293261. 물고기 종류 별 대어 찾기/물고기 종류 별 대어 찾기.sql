7be0adeab3cdbd9c96962553119631272bf3ebff
JI HYUN SIK <wlgustlraaa@naver.com>
2025-04-27 16:10:26 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,27 @@
+WITH MAX_FISH AS (
+    SELECT
+        fi.FISH_TYPE,
+        MAX(fi.LENGTH) AS MAX_LENGTH
+    FROM
+        FISH_INFO as fi
+    GROUP BY
+        fi.FISH_TYPE
+)
+
+SELECT
+    fi.ID AS ID,
+    fn.FISH_NAME AS FISH_NAME,
+    fi.LENGTH AS LENGTH
+FROM
+    FISH_INFO fi
+JOIN
+    MAX_FISH mf
+ON
+    fi.FISH_TYPE = mf.FISH_TYPE
+    AND fi.LENGTH = mf.MAX_LENGTH
+JOIN
+    FISH_NAME_INFO fn
+ON
+    fi.FISH_TYPE = fn.FISH_TYPE
+ORDER BY
+    fi.ID ASC;
